c98c2e38980f3734acd5d45de3da12cdc964659e
Vxrcel <skyeparagon@gmail.com>
2023-09-08 10:15:59 +0100

    Removed extra text

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
-let currentDay = document.getElementById("currentDay");
-const currentTime = document.getElementById('currentTime');
+const dataDay = document.querySelector("p[data-testid='currentDayOfTheWeek']");
+const dataTime = document.querySelector("p[data-testid='currentUTCTime']");
 
 function updateUTCTime() {
     const time = new Date().getTime();
     currentTime.textContent = time
-}
+} dataTime
 function updateUTCDay() {
     const options = {
         timeZone: 'UTC',
         weekday: 'long',
     };
     const day = new Date().toLocaleDateString('en-US', options);
-    currentDay.textContent = day
+    dataDay.textContent = day
 }
 // Update the time initially
 updateUTCTime();
